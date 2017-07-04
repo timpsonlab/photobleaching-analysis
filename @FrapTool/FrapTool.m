06b27c4f52b636e94e6eb86d3962f3c0a6fd1317
Sean Warren <s.warren@garvan.org.au>
2017-07-04 10:52:19 +1000

    Fix type in error handler

--- @FrapTool/FrapTool.m
+++ @FrapTool/FrapTool.m
@@ -33,8 +33,8 @@ classdef FrapTool < handle
                                                 
             obj.play_timer = timer('TimerFcn', @(~,~) obj.IncrementDisplay(), 'ExecutionMode','fixedRate','Period',0.025);
             
-            obj.lh{1} = addlistener(obj.junction_artist,'JunctionsChanged',@(~,~) EC(obj.UpdateKymographList));
-            obj.lh{2} = addlistener(obj.handles.tab_panel,'SelectionChanged',@(~,~) EC(obj.UpdateKymograph));
+            obj.lh{1} = addlistener(obj.junction_artist,'JunctionsChanged',@(~,~) EC(@obj.UpdateKymographList));
+            obj.lh{2} = addlistener(obj.handles.tab_panel,'SelectionChanged',@(~,~) EC(@obj.UpdateKymograph));
         end
         
         function delete(obj)
